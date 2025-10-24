e0f1ec65aa5ee2c47ad2193931979cf80e1f201d
Вячеслав Литвиненко <l24122018@yandex.ru>
2025-10-24 14:55:19 +0300

    Добавление удаления файлов миниатюр скриншотов с диска по сигналу

--- django_base/trades/signals.py
+++ django_base/trades/signals.py
@@ -1,6 +1,8 @@
 import os
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_delete
 from django.dispatch import receiver
+from easy_thumbnails.files import get_thumbnailer
+from easy_thumbnails.models import Thumbnail
 from .models import TradeScreenshot
 
 
@@ -13,4 +15,21 @@ def delete_screenshot_file(sender, instance, **kwargs):
                 os.remove(instance.image.path)
         except (ValueError, OSError):
             # Игнорируем ошибки при удалении файла
+            pass
+
+
+@receiver(post_delete, sender=TradeScreenshot)
+def delete_screenshot_thumbnails(sender, instance, **kwargs):
+    """Удаление миниатюр скриншота при удалении объекта"""
+    if instance.image:
+        try:
+            # Получаем thumbnailer для изображения
+            thumbnailer = get_thumbnailer(instance.image)
+            
+            # Удаляем все миниатюры для этого изображения
+            # Это безопасный способ удаления миниатюр через easy_thumbnails
+            thumbnailer.delete_thumbnails()
+            
+        except (ValueError, OSError, AttributeError):
+            # Игнорируем ошибки при удалении миниатюр
             pass
