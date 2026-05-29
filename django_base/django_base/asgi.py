d26bc8ad97a22444c8995c356ddb346c237e3058
Вячеслав Литвиненко <l24122018@yandex.ru>
2026-05-29 14:13:53 +0300

    feat(infra): подключить websocket routing к ASGI

--- django_base/django_base/asgi.py
+++ django_base/django_base/asgi.py
@@ -1,15 +1,22 @@
-"""
-ASGI config for django_base project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_base.settings')
 
-application = get_asgi_application()
+# Django ASGI app должен быть инициализирован до импорта чего-либо, что
+# использует ORM (consumers/middleware).
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
+from channels.security.websocket import AllowedHostsOriginValidator  # noqa: E402
+
+from accounts.channels_auth import JWTAuthMiddleware  # noqa: E402
+from django_base.routing import websocket_urlpatterns  # noqa: E402
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        JWTAuthMiddleware(URLRouter(websocket_urlpatterns))
+    ),
+})
